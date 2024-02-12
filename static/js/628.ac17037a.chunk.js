410f94a36802b895d7c02b301eaf871279fda8e4
Tobias Skog <t_ott_e@hotmail.com>
2024-02-12 10:57:16 +0100

    Updated folder structure

--- static/js/628.ac17037a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunklabb2_react=self.webpackChunklabb2_react||[]).push([[628],{628:(e,a,b)=>{b.r(a),b.d(a,{default:()=>c});const c={}}}]);
-//# sourceMappingURL=628.ac17037a.chunk.js.map
---
+++ static/js/968.85146872.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklabb2_react=self.webpackChunklabb2_react||[]).push([[968],{968:(e,a,b)=>{b.r(a),b.d(a,{default:()=>c});const c={}}}]);
+//# sourceMappingURL=968.85146872.chunk.js.map
