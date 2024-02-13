6e1208c4f5c95eaeac9e7502b3a23cdcc5e92cb6
Tobias Skog <t_ott_e@hotmail.com>
2024-02-13 13:14:25 +0100

    Updated colors and animations

---
+++ static/js/502.219822ff.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklabb2_react=self.webpackChunklabb2_react||[]).push([[502],{502:(e,a,b)=>{b.r(a),b.d(a,{default:()=>c});const c={}}}]);
+//# sourceMappingURL=502.219822ff.chunk.js.map
--- static/js/968.85146872.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunklabb2_react=self.webpackChunklabb2_react||[]).push([[968],{968:(e,a,b)=>{b.r(a),b.d(a,{default:()=>c});const c={}}}]);
-//# sourceMappingURL=968.85146872.chunk.js.map
