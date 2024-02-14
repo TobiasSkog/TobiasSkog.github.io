6dbd6883acc1520f34b5a7de4ef794e85f4bd124
Tobias Skog <t_ott_e@hotmail.com>
2024-02-14 22:53:14 +0100

    updated CSS

--- static/js/502.219822ff.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunklabb2_react=self.webpackChunklabb2_react||[]).push([[502],{502:(e,a,b)=>{b.r(a),b.d(a,{default:()=>c});const c={}}}]);
-//# sourceMappingURL=502.219822ff.chunk.js.map
